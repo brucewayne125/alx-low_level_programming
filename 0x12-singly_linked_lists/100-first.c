fa551dfbe2cf85f9dcad4e0d5f0d7102e23d8296
brucewayne125 <snowdbat@gmail.com>
2023-08-24 19:59:54 -0700

    task all

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * printMessage - prints a sentence
+ * return: returns null
+ */
+
+void printMessage(void) 
+{
+       	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+int main(void)
+{
+	printMessage();
+	return (0);
+}
