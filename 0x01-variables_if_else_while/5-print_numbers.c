c7d38f49e97715674f315e5693effe8bcbfef407
root <snowdbat@gmail.com>
2025-03-07 09:28:45 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the first nine digits
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
