32ff45d6cbe861896c42712c57d9d8f12c353fef
root <snowdbat@gmail.com>
2025-03-15 09:58:33 -0700

    Debugging.0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  positive_or_negative -  This function Categorizes the result
+ *              from n as positive, negative or zero
+ */
+
+void positive_or_negative(int i)
+{
+        if (i > 0)
+        {
+                printf("%d is positive\n", i);
+        } else if (i < 0)
+        {
+                printf("%d is negative\n", i);
+        } else
+        {
+                printf("%d is zero\n", i);
+        }
+}
