7e8a804ed592d9b39d674eb37de5239df984ef9b
root <snowdbat@gmail.com>
2025-03-16 07:33:57 -0700

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - This function mutiplies two inegers
+ *
+ * @a: parameter for the first number for the function
+ *
+ * @b: parameter for the second number for the function
+ *
+ * Return: the product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
